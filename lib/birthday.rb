666e25fdb40a53f4e39454c11ac90c7aade21060
Arif Cagirici <acagirici23@gmail.com>
2020-02-02 02:09:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda"=> 27}
     birthday_kids.each do |kids_name, age|
+      if age < 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
 end
